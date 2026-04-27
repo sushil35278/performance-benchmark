f2d102f0896b3f69bb0d243881da4296ef15affc
sushil35278 <sigsushil98@gmail.com>
2026-04-28 00:12:50 +0900

    refactor: move all API functions to isolated subdirectories

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,10 @@ const nextConfig = {
   // Required for Vercel to correctly identify the api routes
   rewrites: async () => {
     return [
-      { source: "/api/python", destination: "/api/python.py" },
-      { source: "/api/php", destination: "/api/php.php" },
-      { source: "/api/go", destination: "/api/go.go" },
-      { source: "/api/rust", destination: "/api/rust.rs" },
+      { source: "/api/python", destination: "/api/python/index.py" },
+      { source: "/api/php", destination: "/api/php/index.php" },
+      { source: "/api/go", destination: "/api/go/index.go" },
+      { source: "/api/rust", destination: "/api/rust/index.rs" },
     ];
   },
 };
